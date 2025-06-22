8cb6041432f625a6e18f21c16d25c70bc5859a8d
oscar-benderstone <oscarbenderstone@gmail.com>
2025-06-21 18:53:06 -0600

    Started working on resource queue system

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -2,7 +2,7 @@
 // mod time_management;
 
 use crate::base::AppState;
-use bevy::prelude::*;
+use bevy::{prelude::*, render::view::visibility};
 
 pub struct LevelPlugin;
 
@@ -10,15 +10,60 @@ pub struct LevelPlugin;
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::Level), setup_level)
-            .add_systems(Update, animate_sprite.run_if(in_state(AppState::Level)));
+            .add_event::<WorkerRequest>()
+            .add_systems(
+                Update,
+                (
+                    activate_available_worker,
+                    show_worker,
+                    move_worker,
+                    animate_worker,
+                )
+                    .chain()
+                    .run_if(in_state(AppState::Level)),
+            );
     }
 }
 
 #[derive(Component)]
 struct BarrelTag;
 
+#[derive(Event, Copy, Clone)]
+struct WorkerRequest {
+    position: Vec3,
+}
+
+#[derive(Component, Copy, Clone)]
+struct WorkerTask {
+    position: Vec3,
+}
+
+impl From<Transform> for WorkerRequest {
+    fn from(item: Transform) -> WorkerRequest {
+        WorkerRequest {
+            position: item.translation,
+        }
+    }
+}
+
+impl From<WorkerRequest> for WorkerTask {
+    fn from(item: WorkerRequest) -> WorkerTask {
+        WorkerTask {
+            position: item.position,
+        }
+    }
+}
+
 #[derive(Component)]
-struct WorkerTag;
+struct HomeTag;
+
+#[derive(Component)]
+enum WorkerState {
+    AtHome,
+    ReturningHome,
+    Active,
+    Passive,
+}
 
 #[derive(Component)]
 struct AnimationIndices {
@@ -28,13 +73,6 @@ struct AnimationIndices {
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
-
-#[derive(Component)]
-enum WorkerState {
-    AtHome,
-    Active,
-    Passive,
-}
 
 fn setup_level(
     mut commands: Commands,
@@ -59,12 +97,12 @@ fn setup_level(
     let home_transform = Transform::from_xyz(-300.0, 0.0, 0.0);
     commands
         .spawn((
-            BarrelTag,
+            HomeTag,
             Sprite {
                 image: home,
                 ..default()
             },
-            home_transform.with_scale(Vec3::splat(6.0)),
+            home_transform.with_scale(Vec3::splat(3.0)),
             // Pickable::default(),
         ))
         .observe(check_click);
@@ -77,8 +115,7 @@ fn setup_level(
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 1, last: 6 };
     commands.spawn((
-        WorkerTag,
-        WorkerState::Passive,
+        WorkerState::AtHome,
         Sprite::from_atlas_image(
             gabe,
             TextureAtlas {
@@ -94,16 +131,59 @@ fn setup_level(
     ));
 }
 
-fn check_click(_click: Trigger<Pointer<Pressed>>, mut query: Query<&mut WorkerState>) {
-    for mut state in &mut query {
-        match *state {
-            WorkerState::AtHome | WorkerState::Passive => *state = WorkerState::Active,
-            _ => (),
+fn check_click(
+    _click: Trigger<Pointer<Pressed>>,
+    mut resource_queue: EventWriter<WorkerRequest>,
+    query: Query<(&BarrelTag, &mut Transform)>,
+) {
+    if let Some((_, transform)) = query.iter().next() {
+        resource_queue.write(WorkerRequest::from(*transform));
+    }
+}
+
+fn activate_available_worker(
+    mut commands: Commands,
+    mut workers: Query<(Entity, &mut WorkerState)>,
+    mut task_queue: EventReader<WorkerRequest>,
+) {
+    if let Some(task) = task_queue.read().next() {
+        for (worker_id, mut worker_state) in &mut workers {
+            match *worker_state {
+                WorkerState::AtHome | WorkerState::Passive => {
+                    *worker_state = WorkerState::Active;
+                    commands.entity(worker_id).insert(WorkerTask::from(*task));
+                    break;
+                }
+                _ => continue,
+            }
         }
     }
 }
 
-fn animate_sprite(
+fn show_worker(mut query: Query<(&WorkerState, &mut Visibility)>) {
+    if let Some((WorkerState::Active | WorkerState::Passive, mut visibility)) =
+        query.iter_mut().next()
+    {
+        *visibility = Visibility::Visible
+    }
+}
+
+fn move_worker(mut query: Query<(&mut Transform, &mut WorkerState, &WorkerTask)>) {
+    if let Some((mut transform, state, task)) = query.iter_mut().next() {
+        // TODO: use path following here!
+        if transform.translation == task.position
+            && let WorkerState::ReturningHome = *state
+        {
+            transform.translation.x -= 1.;
+            transform.translation.y -= 0.;
+        } else {
+            transform.translation.x += 1.;
+            transform.translation.y += 0.;
+        }
+    }
+}
+
+fn animate_worker(
     time: Res<Time>,
     mut query: Query<(
         &AnimationIndices,
